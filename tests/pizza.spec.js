ae6a8fa73ccf532023125f54f06e369c636dbcf1
= <=>
2024-10-06 18:37:26 -0600

    feat: implemented first login/logout test

--- tests/pizza.spec.js
+++ tests/pizza.spec.js
@@ -1,66 +1,7 @@
 import { test, expect } from "playwright-test-coverage"
 
 test('purchase with login', async ({ page }) => {
-  await page.route('*/**/api/order/menu', async (route) => {
-    const menuRes = [
-      { id: 1, title: 'Veggie', image: 'pizza1.png', price: 0.0038, description: 'A garden of delight' },
-      { id: 2, title: 'Pepperoni', image: 'pizza2.png', price: 0.0042, description: 'Spicy treat' },
-    ];
-    expect(route.request().method()).toBe('GET');
-    await route.fulfill({ json: menuRes });
-  });
-
-  await page.route('*/**/api/franchise', async (route) => {
-    const franchiseRes = [
-      {
-        id: 2,
-        name: 'LotaPizza',
-        stores: [
-          { id: 4, name: 'Lehi' },
-          { id: 5, name: 'Springville' },
-          { id: 6, name: 'American Fork' },
-        ],
-      },
-      { id: 3, name: 'PizzaCorp', stores: [{ id: 7, name: 'Spanish Fork' }] },
-      { id: 4, name: 'topSpot', stores: [] },
-    ];
-    expect(route.request().method()).toBe('GET');
-    await route.fulfill({ json: franchiseRes });
-  });
-
-  await page.route('*/**/api/auth', async (route) => {
-    const loginReq = { email: 'd@jwt.com', password: 'a' };
-    const loginRes = { user: { id: 3, name: 'Kai Chen', email: 'd@jwt.com', roles: [{ role: 'diner' }] }, token: 'abcdef' };
-    expect(route.request().method()).toBe('PUT');
-    expect(route.request().postDataJSON()).toMatchObject(loginReq);
-    await route.fulfill({ json: loginRes });
-  });
-
-  await page.route('*/**/api/order', async (route) => {
-    const orderReq = {
-      items: [
-        { menuId: 1, description: 'Veggie', price: 0.0038 },
-        { menuId: 2, description: 'Pepperoni', price: 0.0042 },
-      ],
-      storeId: '4',
-      franchiseId: 2,
-    };
-    const orderRes = {
-      order: {
-        items: [
-          { menuId: 1, description: 'Veggie', price: 0.0038 },
-          { menuId: 2, description: 'Pepperoni', price: 0.0042 },
-        ],
-        storeId: '4',
-        franchiseId: 2,
-        id: 23,
-      },
-      jwt: 'eyJpYXQ',
-    };
-    expect(route.request().method()).toBe('POST');
-    expect(route.request().postDataJSON()).toMatchObject(orderReq);
-    await route.fulfill({ json: orderRes });
-  });
+  await setUpServiceMock(page)  
 
   await page.goto('/');
 
@@ -91,4 +32,140 @@ test('purchase with login', async ({ page }) => {
 
   // Check balance
   await expect(page.getByText('0.008')).toBeVisible();
+
+  await page.close()
 });
+
+test('login then logout', async ({ page }) => {
+  await setUpServiceMock(page)  
+
+  await page.goto('/');
+
+  // Login
+  await page.getByRole('link', { name: 'Login' }).click();
+  await page.getByPlaceholder('Email address').click();
+  await page.getByPlaceholder('Email address').fill('d@jwt.com');
+  await page.getByPlaceholder('Email address').press('Tab');
+  await page.getByPlaceholder('Password').fill('a');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await expect(page.getByRole('link', { name: 'KC' })).toBeVisible();
+
+  // Go To Account Page
+
+  await page.getByRole('link', { name: 'KC' }).click();
+  await page.waitForURL('http://localhost:5173/diner-dashboard')
+  await expect(page.getByRole('main')).toContainText('Kai Chen');
+  await expect(page.getByRole('main')).toContainText('d@jwt.com');
+
+  // Log Out
+  await page.getByRole('link', { name: 'Logout' }).click();
+  await page.waitForURL('http://localhost:5173/');
+
+  await expect(page.getByRole('heading')).toContainText('The web\'s best pizza');
+   
+  await page.close(); 
+  
+});
+
+async function setUpServiceMock(page) {
+  await page.route('*/**/api/order/menu', async (route) => {
+    const menuRes = [
+      { id: 1, title: 'Veggie', image: 'pizza1.png', price: 0.0038, description: 'A garden of delight' },
+      { id: 2, title: 'Pepperoni', image: 'pizza2.png', price: 0.0042, description: 'Spicy treat' },
+    ];
+    expect(route.request().method()).toBe('GET');
+    await route.fulfill({ json: menuRes });
+  });
+
+  await page.route('*/**/api/franchise', async (route) => {
+    const franchiseRes = [
+      {
+        id: 2,
+        name: 'LotaPizza',
+        stores: [
+          { id: 4, name: 'Lehi' },
+          { id: 5, name: 'Springville' },
+          { id: 6, name: 'American Fork' },
+        ],
+      },
+      { id: 3, name: 'PizzaCorp', stores: [{ id: 7, name: 'Spanish Fork' }] },
+      { id: 4, name: 'topSpot', stores: [] },
+    ];
+    expect(route.request().method()).toBe('GET');
+    await route.fulfill({ json: franchiseRes });
+  });
+
+  await page.route('*/**/api/auth', async (route) => {
+    const loginReq = { email: 'd@jwt.com', password: 'a' };
+    const loginRes = { user: { id: 3, name: 'Kai Chen', email: 'd@jwt.com', roles: [{ role: 'diner' }] }, token: 'abcdef' };
+    const logoutRes = { message: 'logout successful' };
+    const requestType = route.request().method();
+    if (requestType === 'PUT') {
+      expect(route.request().postDataJSON()).toMatchObject(loginReq);
+      await route.fulfill({ json: loginRes });
+    } else if (requestType === 'DELETE') {
+      await route.fulfill({ json: logoutRes})
+    }
+  });
+
+  await page.route('*/**/api/order', async (route) => {
+    const orderReq = {
+      items: [
+        { menuId: 1, description: 'Veggie', price: 0.0038 },
+        { menuId: 2, description: 'Pepperoni', price: 0.0042 },
+      ],
+      storeId: '4',
+      franchiseId: 2,
+    };
+    const orderRes = {
+      order: {
+        items: [
+          { menuId: 1, description: 'Veggie', price: 0.0038 },
+          { menuId: 2, description: 'Pepperoni', price: 0.0042 },
+        ],
+        storeId: '4',
+        franchiseId: 2,
+        id: 23,
+      },
+      jwt: 'eyJpYXQ',
+    };
+    const allOrdersRes = { 
+      dinerId: 4, 
+      orders: [
+        { 
+          id: 1, 
+          franchiseId: 1, 
+          storeId: 1, 
+          date: '2024-06-05T05:14:40.000Z', 
+          items: [
+            { 
+              id: 1, 
+              menuId: 1, 
+              description: 'Veggie', 
+              price: 0.05 
+            }
+          ] 
+        }, 
+        {
+          items: [
+            { menuId: 1, description: 'Veggie', price: 0.0038 },
+            { menuId: 2, description: 'Pepperoni', price: 0.0042 },
+          ],
+          storeId: '4',
+          franchiseId: 2,
+          date: '2024-06-05T0:14:40.000Z', 
+          id: 23,
+        },
+      ], 
+      page: 1 
+    }
+    
+    if (route.request().method() === 'POST') {
+      expect(route.request().postDataJSON()).toMatchObject(orderReq);
+      await route.fulfill({ json: orderRes });
+    } else if (route.request().method() === 'GET') {
+      expect(await route.request().headerValue('Authorization')).toEqual('Bearer abcdef')
+      await route.fulfill({ json: allOrdersRes });
+    }
+  });
+}
